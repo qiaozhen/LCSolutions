040f9649a354e6a30671fbe5f853236e3ba30d88
qiaozhen <qiaozhen@sogou-inc.com>
2017-07-09 17:51:14 +0800

    commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by qiaozhen on 09/07/2017.
+ */
+
+
+public class Main {
+    public static void main(String[] args) {
+        LongestPalindrome Q = new LongestPalindrome();
+        System.out.print(Q.solution("babad"));
+    }
+}
---
+++ src/Medium/LongestPalindrome.java
@@ -0,0 +1,43 @@
+/**
+ * Created by qiaozhen on 09/07/2017.
+ */
+
+
+public class LongestPalindrome {
+    public String solution(String s) {
+        if (s.length() < 2) {
+            return s;
+        }
+        char[] a = s.toCharArray();
+        int li = 0;
+        int ri = 0;
+
+        for (int i = 0; i < a.length;) {
+            int l = i;
+            int r = i;
+            while (r < a.length && a[r] == a[l]) {
+                r++;
+            }
+            i = r;
+
+            l--;
+            while (r < a.length && l >= 0) {
+                if (a[r] != a[l]) {
+                    break;
+                }
+                r++;
+                l--;
+            }
+
+            r--;
+            l++;
+            if (r-l > ri-li) {
+                li = l;
+                ri = r;
+            }
+        }
+
+        return s.substring(li, ri+1);
+
+    }
+}
